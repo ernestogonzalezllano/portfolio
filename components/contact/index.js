1ff8d5df5afd96c29ba039edb9f1401f3dc1e792
Ernesto <totoeas06@gmail.com>
2020-11-17 21:58:18 -0300

    clean code & add cv download

--- components/contact/index.js
+++ components/contact/index.js
@@ -1,5 +1,6 @@
 import styles from "./index.module.scss"
 
+
 export default function Contact() {
 
     return(<div className={styles.element_contact}>
@@ -12,6 +13,7 @@ export default function Contact() {
                 <a href="https://api.whatsapp.com/send?phone=5493816444487&text=Hola%20Ernesto!%20Vi%20tu%20pagina!%20" target="_blank"><img src="./images/whatsapp.png"></img></a>
                 <a href="https://github.com/ernestogonzalezllano" target="_blank"><img src="./images/github.png"></img></a>
                 <a href="mailto:ernestogonzalezllano@gmail.com" target="_blank"><img src="./images/gmail.png"></img></a>
+                <a href="https://drive.google.com/file/d/16trYt7X3LD3oMXXNrl5ZbR50vAGO_wpz/view" target="_blank"><img src="./images/cv.png"></img></a>
                 </div>
             </div>
     )
