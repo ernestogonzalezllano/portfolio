74fa650b2a96117fdf42289bf0c6722ae3bebf9f
ernestogonzalezpayco <ernesto.gonzalez@payco.co>
2021-07-23 23:29:00 -0300

    continue home section

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,19 +1,27 @@
-import { useState } from "react";
-import { Container, NavBar,Menu, MenuButton, LabelMenu, BrandMenu } from "./styles";
+import { useEffect, useRef, useState } from "react";
+import { Container, NavBar,Menu, MenuButton, LabelMenu, BrandMenu, OtherContainer,Layout,PortfolioButton,AboutButton } from "./styles";
 
-export default function Home(){
-    const[openMenu, setOpenMenu]=useState(false)
+export default function (){
+    const [openMenu, setOpenMenu]=useState(false)
     const [sectionSelected, setSectionSelected]=useState("Home")
+    const homeRef = useRef(null);
+    const otherRef = useRef(null);
+    const references = {
+        Home:homeRef,
+        Contact:otherRef
+    }
     function handleSectionSelected(e){
         e.preventDefault();
         setSectionSelected(e.target.name)
         setTimeout(function () {
             setOpenMenu(false)
         }, 150);
+        window.scrollTo({top: references[e.target.name].current.offsetTop, behavior: 'smooth'});
     }
+
     return(
-        <>
-        <NavBar openMenu={openMenu} >
+        <Layout>
+        <NavBar  openMenu={openMenu} >
             <BrandMenu name="Home" onClick={handleSectionSelected}>Ernesto GLL</BrandMenu>
             <MenuButton openMenu={openMenu} onClick={()=>setOpenMenu(!openMenu)}>
                 <i class="fas fa-bars"></i>
@@ -25,8 +33,17 @@ export default function Home(){
                 <li><LabelMenu isSelected={sectionSelected==="Contact"} name="Contact" onClick={handleSectionSelected}>Contact</LabelMenu></li>
             </Menu>
         </NavBar>
-        <Container>
+        <Container ref={homeRef} >
+            <h1>Hey There!</h1>
+            <h1>I'm Ernesto Gonzalez Llano</h1>
+            <div>
+                <h3>Semi-Senior Web Developer</h3>
+            </div>
+            <PortfolioButton>My portfolio</PortfolioButton>
+            <AboutButton name="About" onClick={handleSectionSelected}>It's Me!</AboutButton>
         </Container>
-        </>
+        <OtherContainer ref={otherRef}>
+        </OtherContainer>
+        </Layout>
     )
 }
--- components/Home/styles.js
+++ components/Home/styles.js
@@ -13,12 +13,78 @@ export const Container = styled.div`
     height: 100vh;
     min-height: 100%;
     width: 100%;
-    position: absolute;
+    position: relative;
     overflow: hidden;
     z-index: 100;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    animation: initLight 15s;
+    h1{
+        background: #80808091;
+        max-width: max-content;
+        margin-left: 1rem;
+        border-radius: 0px 24px 100px 2px;
+        padding: 0 1rem;
+        position: relative;
+        z-index:250;
+        ::before{
+            content: "";
+            border-left: 8px solid #f3c26b;
+            margin-right: .5rem;
+            position: absolute;
+            z-index: 10;
+            display: flex;
+            height: 2.3rem;
+            vertical-align: top;
+            border-radius: 0 18px 0px 14px;
+            width: 11px;
+        }
+    }
+    div{
+        h3{
+            background: black;
+            color: #f3c26b;
+            padding-left: 2rem;
+            position: relative;
+            z-index:500
+        }
+    }
+
+    ::before{
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        z-index:1;
+        backdrop-filter: brightness(0.5) grayscale(.2) blur(5px);
+        content: "";
+    }
     @media only screen and (min-width: ${TABLET}) {
     }
     @media only screen and (min-width:${DESKTOP}){
+        h1{
+            font-size:3rem;
+            ::before{
+                border-left: 12px solid #f3c26b;
+                height:3.6rem;
+                border-radius: 0 4px 0px 22px;
+            }
+        }
+        div{
+            h3{
+                font-size:2rem;
+            }
+        }
+
+    }
+
+    @keyframes initLight{
+        from {
+            box-shadow: inset -94px 125px 110px 225px #000000;
+        }
+        to{
+            box-shadow: inset -94px 125px 110px 225px #00000000;
+        }
     }
 `
 
@@ -106,6 +172,7 @@ export const LabelMenu = styled.a`
     position: relative;
     transition: color .5s;
     color: ${({isSelected})=>isSelected?"#f3c26b":"#000"};
+    font-weight: 500;
     ::before{
         position: absolute;
         height: 2px;
@@ -169,4 +236,72 @@ export const BrandMenu = styled.a`
     font-weight: 500;
     align-self: center;
     cursor: pointer;
+`
+
+export const OtherContainer = styled.div`
+    height:400vh;
+`
+
+export const Layout = styled.div`
+`
+
+export const PortfolioButton = styled.button`
+    position: relative;
+    z-index: 500;
+    background: #f3c26b;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: .5rem 1rem;
+    border-radius: 5px;
+    margin: 0 1rem;
+    box-sizing: border-box;
+    font-weight: 900;
+    font-variant-caps: all-petite-caps;
+    font-size: 1rem;
+    transition: all .4s;
+    :hover{
+        box-shadow: 1px 1px 8px 2px #0000008a;
+        background-color: #fff;
+    }
+    ::before{
+        content: "";
+        width: 2rem;
+        height: 5px;
+        background: black;
+        position: absolute;
+        z-index: 300;
+        left: 0;
+    }
+    @media only screen and (min-width:${DESKTOP}){
+        width: 15rem;
+    }
+
+`
+
+export const AboutButton = styled.button`
+    position: absolute;
+    z-index: 300;
+    bottom: -3rem;
+    left: 50%;
+    background: none;
+    border: none;
+    outline: none;
+    width: min-content;
+    color: white;
+    transition: transform .8s;
+    cursor: pointer;
+    :hover{
+        transform: translateY(-3rem);
+    }
+    ::before{
+        content: "";
+        position: absolute;
+        top: -152%;
+        left: 0;
+        border: 17px solid red;
+        border-top-color: transparent;
+        box-sizing: border-box;
+        border-style: outset;
+    }
 `
