e712cd2ae55a5f726b06a51cd1f96ec6869557bd
ernestogonzalezpayco <ernesto.gonzalez@payco.co>
2021-07-25 13:52:28 -0300

    add about section

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,14 +1,14 @@
 import { useEffect, useRef, useState } from "react";
-import { Container, NavBar,Menu, MenuButton, LabelMenu, BrandMenu, OtherContainer,Layout,PortfolioButton,AboutButton } from "./styles";
+import { ImgAboutContainer,HomeContainer, NavBar,Menu, MenuButton, LabelMenu, BrandMenu, OtherContainer,Layout,PortfolioButton,AboutButton, AboutContainer } from "./styles";
 
-export default function (){
+export default function Home(){
     const [openMenu, setOpenMenu]=useState(false)
     const [sectionSelected, setSectionSelected]=useState("Home")
     const homeRef = useRef(null);
-    const otherRef = useRef(null);
+    const aboutRef = useRef(null);
     const references = {
         Home:homeRef,
-        Contact:otherRef
+        About:aboutRef
     }
     function handleSectionSelected(e){
         e.preventDefault();
@@ -16,7 +16,7 @@ export default function (){
         setTimeout(function () {
             setOpenMenu(false)
         }, 150);
-        window.scrollTo({top: references[e.target.name].current.offsetTop, behavior: 'smooth'});
+        window.scrollTo({top: (references[e.target.name].current.offsetTop-45), behavior: 'smooth'});
     }
 
     return(
@@ -27,13 +27,13 @@ export default function (){
                 <i class="fas fa-bars"></i>
             </MenuButton>
             <Menu openMenu={openMenu} >
-                <li><LabelMenu isSelected={sectionSelected==="Home"} name="Home" onClick={handleSectionSelected}>Home</LabelMenu></li>
                 <li><LabelMenu isSelected={sectionSelected==="About"} name="About" onClick={handleSectionSelected}>About</LabelMenu></li>
                 <li><LabelMenu isSelected={sectionSelected==="Technologies"} name="Technologies" onClick={handleSectionSelected}>Technologies</LabelMenu></li>
+                <li><LabelMenu isSelected={sectionSelected==="Portfolio"} name="Portfolio" onClick={handleSectionSelected}>Portfolio</LabelMenu></li>
                 <li><LabelMenu isSelected={sectionSelected==="Contact"} name="Contact" onClick={handleSectionSelected}>Contact</LabelMenu></li>
             </Menu>
         </NavBar>
-        <Container ref={homeRef} >
+        <HomeContainer ref={homeRef} >
             <h1>Hey There!</h1>
             <h1>I'm Ernesto Gonzalez Llano</h1>
             <div>
@@ -41,8 +41,20 @@ export default function (){
             </div>
             <PortfolioButton>My portfolio</PortfolioButton>
             <AboutButton name="About" onClick={handleSectionSelected}>It's Me!</AboutButton>
-        </Container>
-        <OtherContainer ref={otherRef}>
+        </HomeContainer>
+        <AboutContainer ref={aboutRef}>
+            <ImgAboutContainer>
+                <div>
+                    <img src="https://i.pinimg.com/564x/17/11/e1/1711e194e2efb9254dbb875396854d8b.jpg"></img>
+                </div>
+            </ImgAboutContainer>
+            <div>
+                <label>Ernesto Gonzalez Llano</label>
+                <h2>I'm a semi-senior web developer</h2>
+                <p>About As a web developer I am focused on  learning  to the fullest,  adapting  and facing each and every one of the challenges assigned to me,  contributing  my experience in group and individual projects, both in backend and frontend and nourishing myself with new skills, which together with my own , allow me to increasingly maximize my ability to  transform  programs, companies, realities and lives.</p>
+            </div>
+        </AboutContainer>
+        <OtherContainer >
         </OtherContainer>
         </Layout>
     )
--- components/Home/styles.js
+++ components/Home/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { DESKTOP, TABLET } from "../../utils/consts"
 
 //background: #343436;
-export const Container = styled.div`
+export const HomeContainer = styled.div`
     transition: all 1s;
     background-image: url("./images/desktop.jpg");
     background-repeat: no-repeat;
@@ -171,7 +171,7 @@ export const LabelMenu = styled.a`
     cursor: pointer;
     position: relative;
     transition: color .5s;
-    color: ${({isSelected})=>isSelected?"#f3c26b":"#000"};
+    color: ${({isSelected})=>isSelected?"#f3c26b":"#fff"};
     font-weight: 500;
     ::before{
         position: absolute;
@@ -303,5 +303,103 @@ export const AboutButton = styled.button`
         border-top-color: transparent;
         box-sizing: border-box;
         border-style: outset;
+    }
+    
+`
+
+export const AboutContainer = styled.div`
+    background: #2f2f31;
+    div{
+        padding:3rem;
+        align-self: center;
+        label{
+            color: #f3c26b;
+            position: relative;
+            ::before{
+                content: "";
+                position: absolute;
+                height: 0;
+                width: 1rem;
+                top: .5rem;
+                border: 1px solid;
+                left: -1.5rem;
+            }
+        }
+        h2{
+            color: #fff;
+        }
+        p{
+            color: grey;
+        }
+    }
+    @media only screen and (min-width:${TABLET}){
+        div{
+            padding: 3rem 25%;
+        }
+    }
+    @media only screen and (min-width:${DESKTOP}){
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        div{
+            padding: 3rem;
+        }
+    }
+`
+
+export const ImgAboutContainer = styled.div`
+    div{
+        position:relative;
+        padding:0;
+        max-width: 25rem;
+        margin: 0 auto;
+        img{
+            box-sizing: border-box;
+            width: 100%;
+            box-shadow: -1rem 1rem 0px #f3c26b;
+            position: relative;
+            z-index:10;
+            max-width: 25rem;
+        }
+        ::before{
+            content: "";
+            background-image: url(dots.png);
+            position: absolute;
+            width: 4rem;
+            height: 4rem;
+            top: -1rem;
+            left: -2rem;
+            background-size: 8rem;
+            opacity: .3;
+        }
+        ::after{
+            content: "";
+            background-image: url(dots.png);
+            position: absolute;
+            width: 4rem;
+            height: 4rem;
+            bottom: -2rem;
+            right: -1rem;
+            background-size: 8rem;
+            z-index:0;
+            opacity: .3;
+        }
+    
+        @media only screen and (min-width:${TABLET}){
+            ::before{
+                width: 6rem;
+                height: 6rem;
+                top: -1rem;
+                left: -2rem;
+                background-size: 8rem
+            }
+            ::after{
+                width: 6rem;
+                height: 6rem;
+                bottom: -2rem;
+                right: -2rem;
+                background-size: 8rem
+            }
+        }
+
     }
 `
