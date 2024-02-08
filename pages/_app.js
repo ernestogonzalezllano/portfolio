d29ae139296ea2d6ee6c1da7438ededbebc227f6
Ernesto Gonzalez Llano <ernestogonzalezllano@gmail.com>
2024-02-08 12:32:40 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,12 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+          integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+          crossorigin="anonymous"
+        />
       </Head>
       <main className={montserrat.className}>
         <Component {...pageProps} />
